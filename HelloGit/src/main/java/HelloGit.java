c7c029427d1f08359d2d16654ce1e42e84630637
zlb <657627938@qq.com>
2021-04-28 22:09:53 +0800

    创建分支

--- HelloGit/src/main/java/HelloGit.java
+++ HelloGit/src/main/java/HelloGit.java
@@ -2,6 +2,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello GIT");
         System.out.println("更新1");
-        System.out.println("创建分支dev");
+        System.out.println("创建分支");
     }
 }
